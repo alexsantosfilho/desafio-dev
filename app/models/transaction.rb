c498a7a4a422f7ff6375fc600305fe45d96fc8e4
Alexsandro santos <devalexsandro@gmail.com>
2025-03-17 23:09:56 -0300

    style(transaction): improve readability by using symbols for transaction type keys

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,19 @@
 class Transaction < ApplicationRecord
   belongs_to :store
+
+  TRANSACTION_TYPES = {
+    debit: "Débito",
+    boleto: "Boleto",
+    financing: "Financiamento",
+    credit: "Crédito",
+    loan_receipt: "Recebimento Empréstimo",
+    sales: "Vendas",
+    ted_receipt: "Recebimento TED",
+    doc_receipt: "Recebimento DOC",
+    rent: "Aluguel"
+  }.freeze
+
+  def transaction_type_name
+    TRANSACTION_TYPES[transaction_type] || "Desconhecido"
+  end
 end
