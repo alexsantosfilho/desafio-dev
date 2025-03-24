c662110f7f928ca3f4b053d104db266c04741b13
Alexsandro santos <devalexsandro@gmail.com>
2025-03-23 21:56:21 -0300

    fix(brakeman): security Warnings

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,3 @@
 class AdminController < ActionController::Base
-  http_basic_authenticate_with name: "admin", password: "password"
+  http_basic_authenticate_with name: ENV["http_basic_authenticate_with_name"], password: ENV["http_basic_authenticate_with_password"]
 end
